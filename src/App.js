cc39d7a6f1d3edf1f9c50c0a4f0ae46a8b142608
Roman Elberg <roma.elberg@gmail.com>
2021-12-06 23:58:47 +0300

    lifting staet

--- src/App.js
+++ src/App.js
@@ -2,24 +2,81 @@ import logo from './logo.svg';
 import React from "react";
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.textInput = React.createRef();
-    this.focusTextInput = this.focusTextInput.bind(this);
-  }
-  focusTextInput() {
-      console.log("dom element: ", this.textInput.current);
-    this.textInput.current.focus();
-  }
-  render() {
-    return (
-        <div>
-          <input type={"text"} ref={this.textInput} />
-          <input type={"button"} value={"Focus on the text field"} onClick={this.focusTextInput}/>
-        </div>
-    )
-  }
+
+const scaleNames = {
+    'c': 'Celsium',
+    'f': 'Fahrenheit',
 }
 
-export default App;
+function toCelcius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+}
+function toFahrenheit(celsius) {
+    return(celsius*9/5)+32;
+}
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if(Number.isNaN(input)) {
+        return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output*1000)/1000;
+    return rounded.toString();
+}
+
+function BoilingVerdict(props) {
+    if(props.celsius >= 100) {
+        return <p>Water zakipit</p>
+    }
+    return <p>Water ne zakipit</p>
+}
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+        this.state = {'temperature': '', 'scale': ''}
+    }
+    handleCelsiusChange(temperature) {
+        this.setState({'scale': 'c', temperature});
+    }
+    handleFahrenheitChange(temperature) {
+        this.setState({'scale': 'f', temperature});
+    }
+    render() {
+        const scale = this.state.scale;
+        const temperature = this.state.temperature;
+        const celsius = scale === 'f' ? tryConvert(temperature, toCelcius): temperature;
+        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+        return (
+            <div>
+                <TemperatureInput scale="c" temperature={celsius} onTemperatureChange={this.handleCelsiusChange} />
+                <TemperatureInput scale="f" temperature={fahrenheit} onTemperatureChange={this.handleFahrenheitChange} />
+                <BoilingVerdict celsius={parseFloat(celsius)} />
+            </div>
+        )
+    }
+}
+
+class TemperatureInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e) {
+        this.props.onTemperatureChange(e.target.value);
+    }
+    render() {
+        const temperature = this.props.temperature;
+        const scale = this.props.scale;
+        return (
+            <fieldset>
+                <legend>Enter temp in {scaleNames[scale]}</legend>
+                <input
+                    value={temperature}
+                    onChange={this.handleChange} />
+            </fieldset>
+        )
+    }
+}
+export default Calculator;
