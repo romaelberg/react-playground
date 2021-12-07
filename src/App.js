0bfb0eb0a6717b6a16a4091b7ee53b13ae8d39c1
Roman Elberg <roma.elberg@gmail.com>
2021-12-07 10:41:38 +0300

    component lifecycle

--- src/App.js
+++ src/App.js
@@ -2,81 +2,34 @@ import logo from './logo.svg';
 import React from "react";
 import './App.css';
 
-
-const scaleNames = {
-    'c': 'Celsium',
-    'f': 'Fahrenheit',
-}
-
-function toCelcius(fahrenheit) {
-    return (fahrenheit - 32) * 5/9;
-}
-function toFahrenheit(celsius) {
-    return(celsius*9/5)+32;
-}
-function tryConvert(temperature, convert) {
-    const input = parseFloat(temperature);
-    if(Number.isNaN(input)) {
-        return '';
-    }
-    const output = convert(input);
-    const rounded = Math.round(output*1000)/1000;
-    return rounded.toString();
-}
-
-function BoilingVerdict(props) {
-    if(props.celsius >= 100) {
-        return <p>Water zakipit</p>
-    }
-    return <p>Water ne zakipit</p>
-}
-class Calculator extends React.Component {
+class Clock extends React.Component {
     constructor(props) {
         super(props);
-        this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
-        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
-        this.state = {'temperature': '', 'scale': ''}
+        this.state = {date: new Date()};
     }
-    handleCelsiusChange(temperature) {
-        this.setState({'scale': 'c', temperature});
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        )
     }
-    handleFahrenheitChange(temperature) {
-        this.setState({'scale': 'f', temperature});
-    }
+     componentWillUnmount() {
+        clearInterval(this.timerID);
+     }
+     tick() {
+         console.log(this.timerID);
+        this.setState({
+            date: new Date()
+        })
+     }
+
     render() {
-        const scale = this.state.scale;
-        const temperature = this.state.temperature;
-        const celsius = scale === 'f' ? tryConvert(temperature, toCelcius): temperature;
-        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
-        return (
+        return(
             <div>
-                <TemperatureInput scale="c" temperature={celsius} onTemperatureChange={this.handleCelsiusChange} />
-                <TemperatureInput scale="f" temperature={fahrenheit} onTemperatureChange={this.handleFahrenheitChange} />
-                <BoilingVerdict celsius={parseFloat(celsius)} />
+                <h1>Hello world!</h1>
+                <h2>Now {this.state.date.toLocaleTimeString()}.</h2>
             </div>
         )
     }
 }
-
-class TemperatureInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-    }
-    handleChange(e) {
-        this.props.onTemperatureChange(e.target.value);
-    }
-    render() {
-        const temperature = this.props.temperature;
-        const scale = this.props.scale;
-        return (
-            <fieldset>
-                <legend>Enter temp in {scaleNames[scale]}</legend>
-                <input
-                    value={temperature}
-                    onChange={this.handleChange} />
-            </fieldset>
-        )
-    }
-}
-export default Calculator;
+export default Clock;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Calculator from './App';
+import Clock from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Calculator />
+    <Clock />
   </React.StrictMode>,
   document.getElementById('root')
 );
